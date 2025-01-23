e7a69a14e517cea909d13f42cb13b80f1ca16901
Raj Botre <rajbotre2@gmail.com>
2025-01-23 09:17:29 +0530

    Added Express JS codes

---
+++ JS_Express_Request_Response/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { prototype } = require('node:module')
+const app = express()
+const port = 3000
+app.use(express.static("public"))
+app.get('/', (req , res)=>{
+    res.send("Hello World")
+})
+app.post('/',(req,res)=>{
+   console.log("We are trying post request")
+   res.send("Hello world POST")
+})
+
+app.listen(port, ()=> {
+    console.log(`Example app listening on port ${port}`)
+})
