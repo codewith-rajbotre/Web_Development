a2ca3ab6e19f544dc00f81e2d8ff00c30399168f
Raj Botre <rajbotre2@gmail.com>
2025-10-29 12:50:33 +0530

    DOM Revision

---
+++ JS DOM Examples/script.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('h1DOM');
+element.textContent="Nothing";
+console.log(element);
+
+const elementPara = document.querySelector('.pClass');
+// elementPara.textContent = "The paragraph in JavaScript";
+console.log(elementPara);
